4731225b6ca0030a913fbe8fcd841fbe2ebfe498
Carojas14 <carorojasdevia@gmail.com>
2022-12-01 16:20:53 -0500

    js file and css settings

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(() => {
+
+    window.addEventListener('DOMContentLoaded', (e) => {
+
+        let submit = document.getElementById('btnSubmit');
+
+        submit.addEventListener("click", () => {
+            alert("Coming Soon!\n We are curently working on this form and will launch soon!")
+        });
+
+
+    });
+})();
